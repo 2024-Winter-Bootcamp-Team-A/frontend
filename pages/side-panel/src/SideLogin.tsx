ffcd37ab961d8300d77c35217825a3e17162b061
이현빈 <lhb0107@naver.com>
2025-01-12 12:56:58 +0900

    desgin/feat : 사이드패널 퍼블리싱/구현 1차 완료. 이후 api 연동 및 세부 조정 필요

--- pages/side-panel/src/SideLogin.tsx
+++ pages/side-panel/src/SideLogin.tsx
@@ -14,7 +14,7 @@ const SideLogin = () => {
 
       <div className="text-[#ff5213] text-xl font-normal font-dm-serif mb-2">Password</div>
       <SideInput placeholder="********" isPassWord={true} />
-
+      {/* 일단 로그인 누르먼 소개페이지로 이동하게 만듬 아직 뉴탭 구현 전 */}
       <SideButton name="Login" path="/" />
       <Link to={'/signup'}>
         <div className="text-[#ff5213] text-base font-normal font-dm-serif mt-4 ">Don't have an account?</div>
--- pages/side-panel/tailwind.config.ts
+++ pages/side-panel/tailwind.config.ts
@@ -4,4 +4,12 @@ import type { Config } from 'tailwindcss/types/config';
 export default {
   ...baseConfig,
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'dm-serif': ['"DM Serif Text"', 'serif'], // DM Serif Text 폰트 추가
+        lobster: ['Lobster', 'cursive'], // Lobster 폰트 추가
+      },
+    },
+  },
 } as Config;
